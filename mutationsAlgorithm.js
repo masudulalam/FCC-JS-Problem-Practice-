a121501eb82c115ec03670ffc4c6dce8116df8d8
masudulalam <masuudulalam@gmail.com>
2026-01-10 11:23:55 +0600

    added mutationsAlgorithom problem

---
+++ mutationsAlgorithm.js
@@ -0,0 +1,33 @@
+/**
+ * Implement the Mutations Algorithm
+Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
+
+User Stories:
+
+Create a function named mutation that takes an array as its argument.
+mutation should return true if the string in the first element of the array contains all of the letters of the string in the second element of the array, and false otherwise. For example:
+mutation(["hello", "Hello"]), should return true because all of the letters in the second string are present in the first, ignoring case.
+mutation(["hello", "hey"]) should return false because the string hello does not contain a y.
+mutation(["Alien", "line"]), should return true because all of the letters in line are present in Alien.
+ */
+
+
+//Solution:
+function mutation(arr) {
+  const firstStr = arr[0].toUpperCase();
+  const secondStr = arr[1].toUpperCase();
+  let count = 0
+
+  for(const char of secondStr) {
+    if(firstStr.includes(char)) {
+      count++;
+    } 
+  };  
+  if(count === secondStr.length) {
+    return true;
+  } else {
+      return false;
+    }
+};
+
+console.log(mutation(["Noel", "Ole"]));
