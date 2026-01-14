9eb2bc28a3a4c995e28e4043d7b59ef224cb776e
masudulalam <masuudulalam@gmail.com>
2026-01-14 12:32:51 +0600

    Added the problem Build the Largest Number Finder

---
+++ largestOfAll.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * 
+ * Build the Largest Number Finder
+In this lab, you will build a function that returns an array consisting of the largest number from each provided sub-array.
+
+Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+
+Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
+
+User Stories:
+
+You should create a function largestOfAll that takes an array of arrays as an argument.
+The function should return an array containing the largest number from each sub-array.
+
+ */
+
+// Solution:
+
