40034ed46a2607fc6e9cd324e001ea451c9ebf15
masudulalam <masuudulalam@gmail.com>
2026-01-14 12:20:05 +0600

    Solved the  Build a String Inverter problem

--- reverseString.js
+++ reverseString.js
@@ -16,4 +16,16 @@ The function should return the reversed string.
  */
 
 
-// Solution:
+// Solution: 
+
+function reverseString(str) {
+  let reverseStr = "";
+
+  for(let i = str.length - 1; i >= 0 ; i--) {
+    reverseStr += str[i];
+  }
+  
+  return reverseStr;
+}
+
+console.log(reverseString("hello")) 
