289bf787588a802b529ce03b727e03a9463e9162
masudulalam <masuudulalam@gmail.com>
2026-01-15 20:53:52 +0600

    solved the Largest Number Finder Problem

--- largestOfAll.js
+++ largestOfAll.js
@@ -17,3 +17,25 @@ The function should return an array containing the largest number from each sub-
 
 // Solution:
 
+function largestOfAll(arr) {
+    let larNum = 0;
+    let largestNumArr = [];
+
+    for(let i = 0; i < arr.length; i++) {
+        let insideArr = arr[i];
+        
+        for (let i = 0; i < insideArr.length; i++) {
+            if(insideArr[i] > larNum) {
+                larNum = insideArr[i];
+            }
+        }
+
+        largestNumArr.push(larNum);
+        larNum = 0;
+    }
+
+    return largestNumArr;
+}
+
+
+console.log(largestOfAll([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]));
