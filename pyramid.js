654bbb5b6606b6a5aea713a9d2c52266b6be6a7b
masudulalam <masuudulalam@gmail.com>
2026-01-20 03:50:49 +0600

    Update the if...else block, the block of code is now clean and more readable and another changes in parameter name patternChar instead of patternCharacter also variable name charCount instead of characterCount

--- pyramid.js
+++ pyramid.js
@@ -6,47 +6,61 @@
  * User Stories:
  * You should have a function named pyramid that takes three arguments.
  * 
- * The first argument should be a string representing the pattern character to repeat in your pyramid.
+ * 1. The first argument should be a string representing the pattern character to repeat in your pyramid.
  * 
- * The second argument should be an integer representing the number of rows in the pyramid,
+ * 2. The second argument should be an integer representing the number of rows in the pyramid,
  * 
- * The third argument should be a Boolean value.
+ * 3. The third argument should be a Boolean value.
  * 
- * The pyramid function should return a string in which the pattern character is repeated and        arranged to form a pyramid having the vertex facing upwards when the third argument is false.
+ * 4. The pyramid function should return a string in which the pattern character is repeated and     arranged to form a pyramid having the vertex facing upwards when the third argument is false.
  * 
- * When the third argument is true the pyramid should have the vertex facing downwards.
+ * 5. When the third argument is true the pyramid should have the vertex facing downwards.
  * 
- * The vertex row should have a single pattern character, and each other row should have two pattern characters more than the previous one.
+ * 6. The vertex row should have a single pattern character, and each other row should have two pattern characters more than the previous one.
  * 
- * Each row should start with a number of spaces sufficient to put the center character of each row in the same column and there should not be any spaces at the end of each row.
+ * 7. Each row should start with a number of spaces sufficient to put the center character of each row in the same column and there should not be any spaces at the end of each row.
  * 
- * The pyramid should start and end with a newline character.
+ * 8. The pyramid should start and end with a newline character.
  * 
- * For example, calling pyramid("o", 4, false) should give this output:
+ * 9. For example, calling pyramid("o", 4, false) should give this output: 
+ *          o
+           ooo
+          ooooo
+         ooooooo
  * 
  * 
  */
 
 // Solution:
 
-function pyramid(strCharacter, rows, vertexDownwards) {
+function pyramid(patternChar, rows, vertexDownwards) {
     let result = "\n";
 
-    // if(vertexDownwards) {
-    //     for(let i = 0; i < rows; i++) {
-    //         result += " ".repeat(i) + strCharacter.repeat(2 * (rows - i) - 1) + "\n";
-    //     }
-    // } else {
-    //     for(let i = 0; i < rows; i++) {
-    //         result += " ".repeat(rows - i - 1) + strCharacter.repeat(2 * i + 1) + "\n";
-    //     }
-    // }
-
+    // The code is fine but not more readable and consist repetition
+    /*
+    if(vertexDownwards) {
+        for(let i = 0; i < rows; i++) {
+            result += " ".repeat(i) + strCharacter.repeat(2 * (rows - i) - 1) + "\n";
+        }
+    } else {
+        for(let i = 0; i < rows; i++) {
+            result += " ".repeat(rows - i - 1) + strCharacter.repeat(2 * i + 1) + "\n";
+        }
+    }
+    */
+    
+    // Now code is more readable and clean
     for(let i = 0; i < rows; i++) {
         if(vertexDownwards) {
-            result += " ".repeat(i) + strCharacter.repeat(2 * (rows - i) - 1) + "\n";
+            let spaceCount = " ".repeat(i);
+            let charCount = patternChar.repeat(2 * (rows - i) - 1);
+
+            result += spaceCount + charCount + "\n";
         } else {
-            result += " ".repeat(rows - i - 1) + strCharacter.repeat(2 * i + 1) + "\n";
+            let spaceCount = " ".repeat(rows - i - 1);
+            let charCount = patternChar.repeat(2 * i + 1);
+
+            result += spaceCount + charCount + "\n";
         }  
     }
 
