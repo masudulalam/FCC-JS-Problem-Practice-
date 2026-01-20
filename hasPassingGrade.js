1aec47168729d18875090e8710bb1d713f5b2ec8
masudulalam <masuudulalam@gmail.com>
2026-01-20 10:22:06 +0600

    Added the getGrade() function

--- hasPassingGrade.js
+++ hasPassingGrade.js
@@ -43,8 +43,20 @@ function getAverage(scores) {
     return average;
 }
 
-function getGrade() {
-
+function getGrade(score) {
+    if(score === 100) {
+        return "A+";
+    } else if(score >= 90 && score <= 99) {
+        return "A";
+    } else if(score >= 80 && score <= 89) {
+        return "B";
+    } else if(score >= 70 && score <= 79) {
+        return "C";
+    } else if(score >= 60 && score <= 69) {
+        return "D";
+    } else if(score >= 0 && score <= 59) {
+        return "F";
+    }
 }
 
 function hasPassingGrade() {
@@ -57,3 +69,4 @@ function studentMsg() {
 
 
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+console.log(getGrade(60));
