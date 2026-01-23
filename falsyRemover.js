117b78c3e9f19446fcd6835309e68a916e73690f
masudulalam <masuudulalam@gmail.com>
2026-01-23 19:05:21 +0600

    Solved the Falsy Remover Problem

--- falsyRemover.js
+++ falsyRemover.js
@@ -13,3 +13,14 @@ Hint: Try converting each value to a Boolean.
  */
 
 // Solution:
+
+function bouncer(arr) {
+    let result = arr.filter((element) => !!element)
+
+    return result;
+}
+
+console.log(bouncer([7, "ate", "", false, 9]));
+console.log(bouncer([null, NaN, 1, 2, undefined]));
+console.log(bouncer([false, null, 0, NaN, undefined, ""]));
+console.log(bouncer([]));
